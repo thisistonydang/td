ef9db78a27286ebc5d7d2d146de0b5523766fb99
Tony Dang <tony@tonydang.com>
2025-06-26 10:26:19 -0700

    update doc

--- cmd/rg.go
+++ cmd/rg.go
@@ -14,14 +14,15 @@ func init() {
 }
 
 var rgCmd = &cobra.Command{
-	Use:   "rg FILE_EXTENSION(S) SEARCH_STRING",
-	Short: "Find files containing text, filtered by file extensions",
+	Use:   "rg [FILE_EXTENSION(S)] SEARCH_STRING",
+	Short: "Find files containing text, optionally filtered by file extensions",
 	Long: `A streamlined wrapper around ripgrep that searches for files containing a specific string, 
-filtered by file extensions. Returns a list of matching filenames rather than file contents, 
+optionally filtered by file extensions. Returns a list of matching filenames rather than file contents, 
 making it ideal for quickly locating files that contain specific code or text patterns.
 
-Supports single extensions (go) or multiple comma-separated extensions (go,ts,ex) to narrow 
-your search scope efficiently.`,
+Supports single extensions (go) or multiple comma-separated extensions (go,ts,ex) to narrow your search scope efficiently.
+
+If no file extensions are provided, all files types will be searched.`,
 	Example: `  td rg go string_to_search_for
   td rg go,ts,ex multiple_comma_separated_extensions
   td rg go "string with spaces"
