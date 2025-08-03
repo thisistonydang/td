9632166cddcbc29de148a1dd66bffd610dd8ad64
Tony Dang <tony@tonydang.com>
2025-08-03 15:39:56 -0700

    cmd declaration

--- internal/cmd/dev.go
+++ internal/cmd/dev.go
@@ -15,3 +15,32 @@ var (
 	ProjectType string
 )
 
+var devCmd = &cobra.Command{
+	Use:   "dev",
+	Short: "Start development environment",
+	Long: `Start a development environment based on the current directory's project type. 
+
+By default, if a .env file is present, it is loaded before starting the development environment.
+This can be skipped with the --skip-env flag.
+
+Project types are auto-detected by the presence of the following files:
+- Go - go.mod
+- Elixir (Phoenix) - mix.exs
+- JavaScript (pnpm) - package.json
+
+To force a project type, rather than auto-detecting, use the --type flag. (e.g. --type go)
+
+The following commands are run for each project type:
+- Go - air
+- Elixir (Phoenix) - iex -S mix phx.server
+- JavaScript (pnpm) - pnpm dev
+`,
+	Example: `  td dev
+  td dev --skip-env
+  td dev --type go`,
+	Args: cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		startDevEnvironment()
+	},
+}
+
